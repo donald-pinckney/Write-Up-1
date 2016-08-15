2b84df174cd1b0745daa4527f22410884e23f5dc
Donald Pinckney <donald_pinckney@icloud.com>
2016-08-14 19:22:51 -0700

    Create problem1

--- problem1.swift
+++ problem1.swift
@@ -1,4 +1,6 @@
-import Foundation
+// For this problem, you need to print the text "Hello, World!" (without the quotes).
+// This serves to see if you have your environment setup properly, and can successfully submit homework.
+// Once you complete this problem, submit it IMMEDIATELY, so that I know everyone has a working environment and can submit homework.
+// If you run into problems, please contact me ASAP (donald_pinckney@icloud.com)
 
-// For this problem, you need to print the text "Hello, World!"
-print("my text here")
+print("my text goes here")
