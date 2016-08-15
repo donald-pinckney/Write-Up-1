d316f0cf2841e5c5667985346b970f1be6709957
Donald Pinckney <donald_pinckney@icloud.com>
2016-08-14 19:55:30 -0700

    Add testing data for problems 1 and 2.

--- problem1.swift
+++ problem1.swift
@@ -1,6 +1,3 @@
-// For this problem, you need to print the text "Hello, World!" (without the quotes).
-// This serves to see if you have your environment setup properly, and can successfully submit homework.
-// Once you complete this problem, submit it IMMEDIATELY, so that I know everyone has a working environment and can submit homework.
-// If you run into problems, please contact me ASAP (donald_pinckney@icloud.com)
+// See description in problem1.README
 
 print("my text goes here")
