243f16e60212e319b0b14b02b11e18408bd7ea89
Donald Pinckney <donald_pinckney@icloud.com>
2016-09-15 23:53:42 -0700

    Update problem2.swift

--- problem2.swift
+++ problem2.swift
@@ -1,2 +1,7 @@
 // See description in problem2.README
 
+// This reads in the 4 numbers.
+let n1 = Double(readLine()!)!
+let n2 = Double(readLine()!)!
+let n3 = Double(readLine()!)!
+let n4 = Double(readLine()!)!
