f2ed9e7cfa16f11acae897dea176112934d7eef2
rellimylime <ermiller95616@gmail.com>
2016-09-19 01:07:44 +0000

    Successfully prints all four values

--- problem2.swift
+++ problem2.swift
@@ -1,7 +1,17 @@
 // See description in problem2.README
-
-// This reads in the 4 numbers.
+import Foundation
+print("Please enter four positive numbers")
 let n1 = Double(readLine()!)!
 let n2 = Double(readLine()!)!
 let n3 = Double(readLine()!)!
 let n4 = Double(readLine()!)!
+
+let sum = n1 + n2 + n3 + n4
+let mean = sum / 4
+let multiplication = n1 * n2 * n3 * n4
+let geomean = pow(multiplication, 0.25)
+
+print("Sum: \(sum)") 
+print("Mean: \(mean)") 
+print("Multiplication: \(multiplication)") 
+print("Geometric Mean: \(geomean)")
