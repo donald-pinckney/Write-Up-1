3a11f11175ba698caaeaa02a0c4b4d0df919e953
javaraser <darting.thoughts@gmail.com>
2016-09-19 04:28:03 +0000

    Solve Problem 2

--- problem2.swift
+++ problem2.swift
@@ -1,7 +1,21 @@
 // See description in problem2.README
 
 // This reads in the 4 numbers.
+import Foundation
+
+print("Enter thy numebrs")
+
 let n1 = Double(readLine()!)!
 let n2 = Double(readLine()!)!
 let n3 = Double(readLine()!)!
 let n4 = Double(readLine()!)!
+
+let sum = n1 + n2 + n3 + n4
+let mean = sum / 4
+let multiplication = n1 * n2 * n3 * n4
+let geometricMean = pow(multiplication, 1/4)
+
+print(sum)
+print(mean)
+print(multiplication)
+print(geometricMean)
