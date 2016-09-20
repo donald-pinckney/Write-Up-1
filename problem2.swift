e84d0b19f65c2ea638484570401a0132c018cf50
Brian <bgl1412@gmail.com>
2016-09-19 19:40:28 -0700

    Update problem2.swift

--- problem2.swift
+++ problem2.swift
@@ -1,7 +1,18 @@
-// See description in problem2.README
+import Foundation
+print("Enter 4 positive numbers.")
 
-// This reads in the 4 numbers.
 let n1 = Double(readLine()!)!
 let n2 = Double(readLine()!)!
 let n3 = Double(readLine()!)!
 let n4 = Double(readLine()!)!
+
+let sum = n1 + n2 + n3 + n4
+let multiplication = n1 * n2 * n3 * n4
+let mean = (n1 + n2 + n3 + n4) / 4
+let geometricmean = pow(n1 * n2 * n3 * n4, 1/4)
+
+print (Sum = \(sum)")
+print (Multiplication = \(multiplication)")
+print (Mean = \(mean)")
+print ("Geometric Mean = \(geometricmean)")
+
