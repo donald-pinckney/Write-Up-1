4eb99c139ce5716acec1042bf7073899d025eab7
niraqw15 <niraqw@gmail.com>
2016-09-20 06:05:07 +0000

    Solve problems 1 and 2

--- problem2.swift
+++ problem2.swift
@@ -1,7 +1,19 @@
 // See description in problem2.README
 
+import Foundation
+
 // This reads in the 4 numbers.
 let n1 = Double(readLine()!)!
 let n2 = Double(readLine()!)!
 let n3 = Double(readLine()!)!
 let n4 = Double(readLine()!)!
+
+let nSum: Double = n1 + n2 + n3 + n4
+let nMean: Double = nSum / 4
+let nPro: Double = n1 * n2 * n3 * n4
+let nGeo: Double = pow(nPro, 0.25)
+
+print("Sum = ", nSum)
+print("Mean = ", nMean)
+print("Product = ", nPro)
+print("Geometric Mean = ", nGeo)
