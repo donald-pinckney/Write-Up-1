38673a1bc1579aca9d5c8df6ebdec4bd4f86c1ff
Glenn LeBlanc <gl3nnleblanc@gmail.com>
2016-09-20 01:18:25 +0000

    Complete

--- problem2.swift
+++ problem2.swift
@@ -1,7 +1,14 @@
 // See description in problem2.README
-
+import Foundation
 // This reads in the 4 numbers.
 let n1 = Double(readLine()!)!
 let n2 = Double(readLine()!)!
 let n3 = Double(readLine()!)!
 let n4 = Double(readLine()!)!
+
+let sum      = n1 + n2 + n3 + n4
+let mean     = sum / 4
+let product  = n1 * n2 * n3 * n4
+let geometmn = pow(product, 0.25)
+
+print("\(sum)\n\(mean)\n\(product)\n\(geometmn)")
