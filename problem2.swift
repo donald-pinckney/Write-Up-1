1ee1e93ed747ebcbcb64a1de3884de868301befa
Ionatan Soule <ionatan98@gmail.com>
2016-09-19 17:46:09 -0700

    Solve problems 1 and 2

--- problem2.swift
+++ problem2.swift
@@ -5,3 +5,13 @@ let n1 = Double(readLine()!)!
 let n2 = Double(readLine()!)!
 let n3 = Double(readLine()!)!
 let n4 = Double(readLine()!)!
+
+import Foundation
+
+print(n1 + n2 + n3 + n4)
+print((n1 + n2 + n3 + n4) / 4.0)
+print(n1 * n2 * n3 * n4)
+print(pow((n1 * n2 * n3 * n4) , (1 / 4)))
+
+
+
