3cbe7216704104208c6a36cc7082be33949d61d9
Gemma_B <gemma@bertain.net>
2016-09-20 04:46:38 +0000

    final commit for Write-Up-1

--- problem2.swift
+++ problem2.swift
@@ -1,7 +1,16 @@
 // See description in problem2.README
+import Foundation
 
 // This reads in the 4 numbers.
 let n1 = Double(readLine()!)!
 let n2 = Double(readLine()!)!
 let n3 = Double(readLine()!)!
 let n4 = Double(readLine()!)!
+let sum = n1 + n2 + n3 + n4
+let mean = (n1 + n2 + n3 + n4) / 4
+let multiplication = n1 * n2 * n3 * n4
+let geometric_mean = pow((n1 * n2 * n3 * n4),(1/4))
+print(sum)
+print(mean)
+print(multiplication)
+print(geometric_mean)
