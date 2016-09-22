6f1325e4ea464373455d3b26deb08ccd406da378
Donald Pinckney <donald_pinckney@icloud.com>
2016-09-22 10:38:46 -0700

    Update .travis.install_swift.sh

--- .travis.install_swift.sh
+++ .travis.install_swift.sh
@@ -5,7 +5,7 @@ sudo apt-get -yq install clang libicu-dev
 
 # Install Swift
 mkdir ~/bin
-swiftURL="https://swift.org/builds/swift-3.0-release/ubuntu1510/swift-3.0-RELEASE/swift-3.0-RELEASE-ubuntu15.10.tar.gz"
+swiftURL="https://swift.org/builds/swift-3.0-release/ubuntu1404/swift-3.0-RELEASE/swift-3.0-RELEASE-ubuntu14.04.tar.gz"
 swiftFile=$(basename $swiftURL)
 swiftDir="${swiftFile/.tar.gz/}"
 wget -nv $swiftURL
