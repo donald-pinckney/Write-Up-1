34abb9871220690960c067359d5710d26f5af785
Nathan Solomon <nathansolomon1678@gmail.com>
2016-10-01 19:14:51 +0000

    did problems 1 and 2

--- problem2.swift
+++ problem2.swift
@@ -1,3 +1,4 @@
+import Foundation
 // See description in problem2.README
 
 // This reads in the 4 numbers.
@@ -5,3 +6,13 @@ let n1 = Double(readLine()!)!
 let n2 = Double(readLine()!)!
 let n3 = Double(readLine()!)!
 let n4 = Double(readLine()!)!
+
+let sum = n1 + n2 + n3 + n4
+let mean = sum / 4
+let multiplication = n1 * n2 * n3 * n4
+let geometricMean = pow(multiplication, 1/4)
+
+print (sum)
+print (mean)
+print (multiplication)
+print (geometricMean)
